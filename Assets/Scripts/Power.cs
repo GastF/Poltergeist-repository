acb14f6aa7676b0707d747c936af1d3736ab7ede
PabloVallejos <pablovallejosmil@hotmail.com>
2023-04-25 15:39:38 -0300

    Añadido barra de energía

--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -6,12 +6,20 @@ using UnityEngine.UI;
 public class Power : MonoBehaviour
 {
     public Text txt;
+    public Slider sld;
     public int limit; //Limite de energía
     public float pwr; //Energía actual
+
+    private void Start()
+    {
+        sld = FindObjectOfType<Slider>();
+        sld.maxValue = limit;
+    }
 
     private void Update()
     {
         txt.text = "Power: " + pwr;
+        sld.value = pwr;
 
         //Carga de energía
         if (pwr < limit)
